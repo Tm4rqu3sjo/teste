f738aa7236aad1e35876ab0d5663767a4e4a8424
Tm4rqu3sjo <126785824+Tm4rqu3sjo@users.noreply.github.com>
2023-03-02 10:48:33 -0300

    primeiro commit
    Adicionei os arquivos

---
+++ src/jo0203.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class jo0203 {
+
+    public static void main(String [] args){
+
+        Scanner scan = new Scanner(System.in);
+
+    System.out.printf("Hello, world");
+
+
+
+    }
+}
